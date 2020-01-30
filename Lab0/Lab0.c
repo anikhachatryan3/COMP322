5ce88988820c0543a246d2bfd34b25c5a256daae
Ani Khachatryan <anikhachatryan@Anis-MacBook-Pro.local>
2020-01-29 18:17:36 -0800

    This is my initial commit.

---
+++ Lab0/Lab0.c
@@ -0,0 +1,28 @@
+//Name: Ani Khachatryan
+//Date: 25 January 2020
+//Class: COMP322/L
+//Assignment: Lab 0 - Making Sense of Ones and Zeros
+//Purpose: To write a command line tool that will read a series of 1’s and 0’s from a file 
+//         that consists of: the original value, the corresponding ASCII character, the 
+//         corresponding decimal value, and the value of either EVEN or ODD depending on 
+//         the number of 1's set.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main() {
+
+    printf("\nOriginal   ASCII   Decimal   Parity   T. Error\n");
+    printf("--------  -------  --------  -------  ---------\n");
+    // printf("                    %f                         \n");
+
+}
+
+
+//Compile:
+// gcc -c file.c -Wall -Wextra
+// gcc -o file file.o -lm ??
+// ./file
