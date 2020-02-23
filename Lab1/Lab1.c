a7154c4115f818977b1a2265b1e390725af11f4e
Ani Khachatryan <anikhachatryan@Anis-MacBook-Pro.local>
2020-02-22 22:35:38 -0800

    mostly done the code is mostly done. i only need the user, cuser, sys, and csys to work correctly

---
+++ Lab1/Lab1.c
@@ -0,0 +1,86 @@
+//Name: Ani Khachatryan
+//Date: 19 February 2020
+//Assignment: Lab1 - Time and Forking Children
+//Purpose: write a command line tool that reports basic process identification 
+//         information, and the associated times for running the command line tool.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+// #include <sys/types.h> 
+#include <sys/errno.h>
+#include <sys/times.h>
+
+//method for child process
+void childProcess(pid_t parent, pid_t child) {
+    //print pid and ppid
+    printf("PPID: %d, PID: %d\n", parent, child);
+    //exit process
+    exit(EXIT_SUCCESS);
+}
+
+//method to print user and sys
+void childTime(clock_t user, clock_t system) {
+    printf("USER: %d, SYS: %d\n", (int)user, (int)system);
+}
+
+//method for parent process
+void parentProcess(pid_t parent, pid_t pid, pid_t child, int status) {
+    //print pid, ppid, cpid, and retVal
+    printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", parent, pid, child, WIFEXITED(status)); //WEXITSTATUS(status)?
+}
+
+//method to print cuser and csys
+void parentTime(clock_t user, clock_t system) {
+    printf("CUSER: %d, CSYS: %d\n", (int)user, (int)system);
+}
+
+//main function
+int main() {
+    //create time variable
+    time_t seconds;
+    //create waiting variable
+    pid_t waiting;
+    //create status variable
+    int status;
+    time(&seconds);
+    //print start time
+    printf("START: %ld\n", seconds);
+    //create child process (fork)
+    pid_t child = fork();
+    //create struct for user and system times
+    struct tms clocks;
+    //if child = 0
+    if(child == 0) {
+        //make process sleep for 10 seconds
+        sleep(10);
+        //do child process
+        childProcess(getppid(), getpid());
+    }
+    //if child is greater than 0
+    else if(child > 0) {
+        //is parent (wait for child to finish)
+        waiting = waitpid(child, &status, WUNTRACED);
+        //do parent process
+        parentProcess(getppid(), getpid(), child, status);
+        times(&clocks);
+        //get child time
+        childTime(clocks.tms_utime, clocks.tms_stime);
+        //get parent time
+        parentTime(clocks.tms_cutime, clocks.tms_cstime);
+    }
+    //otherwise
+    else {
+        //print error number (errno)
+        printf("Error Number: %d\n", errno);
+        //print "error"
+        printf("Forking Failed.\n");
+        return 1;
+    }
+    time(&seconds);
+    printf("STOP: %ld\n", seconds);
+
+    return 0;
+}
---
+++ Lab1/readme.txt
@@ -0,0 +1,8 @@
+README Document:
+
+02/22/2020: I just started working on the project. I created the Lab1.c file, the README 
+            file, the makefile, and the AniKhachatryan file to submit. So far, I have gotten
+            the child process and parent process to both work successfully. I just need to 
+            get the USER, CUSER, SYS, and CSYS to print correctly along with making sure if 
+            the RETVAL is correct.
+            Status: working; mostly complete.
