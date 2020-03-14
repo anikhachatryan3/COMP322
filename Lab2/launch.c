91f24ac010dee3912de09f30f667111b6bd83e57
Ani Khachatryan <anikhachatryan@Anis-MacBook-Pro.local>
2020-03-13 23:03:07 -0700

    halfway there finished launch file, did some of the tube file

--- Lab2/launch.c
+++ Lab2/launch.c
@@ -15,43 +15,45 @@
 //method to print the cpid
 void printCPID(pid_t child) {
     //print cpid
-    fprintf(stderr, "CPID: %d\n", child);
+    fprintf(stderr, "$$ = %d\n", child);
 }
 
 //method to print retval
 void printRetVal(int status) {
     //print retval
-    printf("RETVAL: %d\n", status);
+    fprintf(stderr, "$? = %d\n", status);
 }
 
 //main method
 int main(int argc, char** argv) {
-    //create i variable to use in for loop
-    int i;
-    //create status variable
-    int status;
-    //fork and store in child variable
-    pid_t child = fork();
-    //create waiting variable
-    pid_t waiting;
-    //if argc is not 0 (or empty)
+    //if argc is greater than 0 (not empty)
     if(argc > 0) {
+        //create i variable to use in for loop
+        int i;
+        //create status variable
+        int status;
+        //fork and store in child variable
+        pid_t child = fork();
+        //create waiting variable
+        pid_t waiting;
+        //create arg array variable
+        char* argArr[argc];
         //if child is 0
         if(child == 0) {
-            //do child stuff
-            // for(i = 0; i < argc; i++) {
-                printf("do something\n");
-            //     execve(argv[i]);
-            // }
+            //while i is less than argc
+            for(i = 1; (i - 1) < argc; i++) {
+                argArr[i-1] = argv[i];
+            }
+            execve(argv[1], argArr, NULL);
         }
         //if the child is a positive number
         else if(child > 0) {
-            //wait for child to finish
-            waiting = waitpid(child, &status, WUNTRACED);
             //print the cpid
             printCPID(child);
-            //print the return value of the child
-            printRetVal(status);
+            //wait for child to finish
+            waiting = waitpid(child, &status, WUNTRACED);
         }
+        //print the return value of the child
+        printRetVal(status);
     }
 }
--- Lab2/tube.c
+++ Lab2/tube.c
@@ -7,7 +7,60 @@
 //         child and then the second child on stderr.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main() {
-    printf("There\n");
+//method to print CPID of both children
+void printCPIDs(pid_t child1, pid_t child2) {
+    //print cpid of child1
+    fprintf(stderr, "CPID1: %d\n", child1);
+    //print cpid of child2
+    fprintf(stderr, "CPID: %d\n", child2);
+}
+
+//method to print retval
+void printRetVal(int status1, int status2) {
+    //print retval for child1
+    printf("RETVAL: %d\n", status1);
+    //print retval for child2
+    printf("RETVAL: %d\n", status2);
+}
+//method to use dup2()
+// void doDup2() {
+//     // dup2();
+// }
+
+// //main method
+int main(int argc, char** argv) {
+    if(argc > 0) {
+        //pipe
+        int pipe[argc];
+        //create status1 and status2 variables
+        int status1 = -1;
+        int status2 = -1;
+        //create first child process using fork() and store in child1
+        pid_t child1 = fork();
+        //create second child process using fork() and store in child2
+        pid_t child2 = fork();
+        //create wait1 and wait2 variables
+        pid_t wait1;
+        pid_t wait2;
+        //if both children are a positive number
+        if(child1 == 0 && child2 == 0) {
+            printf("Child process\n");
+        }
+        else if((child1 > 0) && (child2 > 0)) {
+            //wait for child process 1 and 2 to finish
+            wait1 = waitpid(child1, &status1, WUNTRACED);
+            wait2 = waitpid(child2, &status2, WUNTRACED);
+            //print cpids of both children
+            printCPIDs(child1, child2);
+            // close();
+        }
+        //print return value of both children
+        printRetVal(status1, status2);
+    }
 }
